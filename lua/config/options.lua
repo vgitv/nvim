fcf449a2584adf8c89af80601eaf5b0b997baf34
vgitv <33827923+vgitv@users.noreply.github.com>
2024-12-11 17:05:59 +0100

    indent

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -44,9 +44,7 @@ vim.opt.wrap = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
+vim.schedule(function() vim.opt.clipboard = 'unnamedplus' end)
 
 -- round indent to multiple of shiftwidth
 vim.opt.shiftround = true

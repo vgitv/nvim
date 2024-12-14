10d5cec13d3cdb663fe716acb54ba7ca533dbe38
vgitv <33827923+vgitv@users.noreply.github.com>
2024-12-14 10:46:03 +0100

    md maps

--- lua/plugins/bullets.vim.lua
+++ lua/plugins/bullets.vim.lua
@@ -3,6 +3,6 @@ return {
     -- only load this plugin for markdown filetype
     ft = 'markdown',
     keys = {
-        { '<LocalLeader>x', ':ToggleCheckbox<CR>', desc = 'Toggle check box' },
+        { '<LocalLeader>\\', ':ToggleCheckbox<CR>', desc = 'Toggle check box' },
     },
 }

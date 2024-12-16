6898c31ed553347d43c4010503d832a1644b7977
vgitv <33827923+vgitv@users.noreply.github.com>
2024-12-16 08:26:43 +0100

    init arg for colorscheme

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -3,45 +3,44 @@ return {
     "rebelot/kanagawa.nvim",
     lazy = false,  -- make sure we load this during startup if it is your main colorscheme
     priority = 1000,  -- make sure to load this before all the other start plugins
-    config = function()
-        require('kanagawa').setup {
-            compile = false,
-            undercurl = true,
-            commentStyle = { italic = true },
-            functionStyle = { bold = true},  -- more visible function for bash scripts
-            keywordStyle = { italic = true},
-            statementStyle = { bold = true },
-            typeStyle = {},
-            transparent = false,
-            dimInactive = false,  -- dim inactive window `:h hl-NormalNC`
-            terminalColors = true,  -- define vim.g.terminal_color_{0,17}
-            colors = {  -- add/modify theme and palette colors
-                palette = {},
-                theme = {
-                    wave = {
-                        ui = {
-                            bg_gutter = "none",  -- Remove the background of LineNr, {Sign,Fold}Column and friends
-                            bg_p1 = "#16161d",  -- colorcolumn
-                            bg_p2 = "#16161d",  -- cursorline color
-                        },
+    opts = {
+        compile = false,
+        undercurl = true,
+        commentStyle = { italic = true },
+        functionStyle = { bold = true},  -- more visible function for bash scripts
+        keywordStyle = { italic = true},
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false,
+        dimInactive = false,  -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true,  -- define vim.g.terminal_color_{0,17}
+        colors = {  -- add/modify theme and palette colors
+            palette = {},
+            theme = {
+                wave = {
+                    ui = {
+                        bg_gutter = "none",  -- Remove the background of LineNr, {Sign,Fold}Column and friends
+                        bg_p1 = "#16161d",  -- colorcolumn
+                        bg_p2 = "#16161d",  -- cursorline color
                     },
-                    lotus = {},
-                    dragon = {},
-                    all = {}
                 },
+                lotus = {},
+                dragon = {},
+                all = {}
             },
-            overrides = function()  -- add/modify highlights
-                return {
-                    IblIndent = { fg = "#2a2a37"},  -- Indent char for indent-blankline plugin
-                }
-            end,
-            theme = "wave",  -- Load "wave" theme when 'background' option is not set
-            background = {  -- map the value of 'background' option to a theme
-                dark = "wave",   -- try "dragon" !
-                light = "lotus"
-            },
-        }
-        -- load the colorscheme here
-        vim.cmd([[colorscheme kanagawa]])
-    end,
+        },
+        overrides = function()  -- add/modify highlights
+            return {
+                IblIndent = { fg = "#2a2a37"},  -- Indent char for indent-blankline plugin
+            }
+        end,
+        theme = "wave",  -- Load "wave" theme when 'background' option is not set
+        background = {  -- map the value of 'background' option to a theme
+            dark = "wave",   -- try "dragon" !
+            light = "lotus"
+        },
+    },
+    init = function()
+        vim.cmd.colorscheme 'kanagawa'
+    end
 }

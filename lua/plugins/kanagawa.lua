9c4d8e77b081d2916f1f29e50772bf37bab34ca6
vgitv <33827923+vgitv@users.noreply.github.com>
2024-12-17 13:14:22 +0100

    diagnostics colors

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -29,9 +29,19 @@ return {
                 all = {}
             },
         },
-        overrides = function()  -- add/modify highlights
+        overrides = function(colors)  -- add/modify highlights
+            local theme = colors.theme
+            local makeDiagnosticColor = function(color)
+                local c = require("kanagawa.lib.color")
+                return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
+            end
+
             return {
                 IblIndent = { fg = "#2a2a37"},  -- Indent char for indent-blankline plugin
+                DiagnosticVirtualTextHint  = makeDiagnosticColor(theme.diag.hint),
+                DiagnosticVirtualTextInfo  = makeDiagnosticColor(theme.diag.info),
+                DiagnosticVirtualTextWarn  = makeDiagnosticColor(theme.diag.warning),
+                DiagnosticVirtualTextError = makeDiagnosticColor(theme.diag.error),
             }
         end,
         theme = "wave",  -- Load "wave" theme when 'background' option is not set

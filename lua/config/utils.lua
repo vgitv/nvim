4bea8f7beee50cd9de4d70c302ecfc3c40572688
vgitv <33827923+vgitv@users.noreply.github.com>
2024-12-20 20:38:05 +0100

    alternative toggle terminal

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -100,3 +100,77 @@ function FormatCurrentFile()
         print('Format command not implemeted yet')
     end
 end
+
+
+local function create_floating_window(opts)
+    opts = opts or {}
+    local width = opts.width or math.floor(vim.o.columns * 0.8)
+    local height = opts.height or math.floor(vim.o.lines * 0.8)
+
+    -- Calculate the position to center the window
+    local col = math.floor((vim.o.columns - width) / 2)
+    local row = math.floor((vim.o.lines - height) / 2)
+
+    -- Create a buffer (no file, scratch buffer)
+    local buf = vim.api.nvim_create_buf(false, true)
+
+    -- Define window configuration
+    local win_config = {
+        relative = "editor",
+        width = width,
+        height = height,
+        col = col,
+        row = row,
+        style = "minimal",
+        border = "rounded",
+    }
+
+    local win = vim.api.nvim_open_win(buf, true, win_config)
+
+    return { buf = buf, win = win }
+end
+
+local state = {
+    main_terminal = {
+        buf = -1,
+        win = -1,
+    }
+}
+
+local create_window_below = function(opts)
+    opts = opts or {}
+    local height = opts.height or math.floor(vim.o.lines * 0.35)
+
+    local buf = nil
+    if vim.api.nvim_buf_is_valid(opts.buf) then
+        buf = opts.buf
+    else
+        buf = vim.api.nvim_create_buf(false, true)
+    end
+
+    local win_config = {
+        split = 'below',
+        win = 0,
+        height = height,
+    }
+
+    local win = vim.api.nvim_open_win(buf, true, win_config)
+
+    return { buf = buf, win = win }
+end
+
+local toggle_terminal = function()
+    if not vim.api.nvim_win_is_valid(state.main_terminal.win) then
+        state.main_terminal = create_window_below { buf = state.main_terminal.buf }
+        if vim.bo[state.main_terminal.buf].buftype ~= 'terminal' then
+            vim.cmd.terminal()
+            vim.bo.buflisted = false
+            vim.opt_local.winhighlight = 'Normal:LuaTerminalNormal'
+        end
+    else
+        vim.api.nvim_win_hide(state.main_terminal.win)
+    end
+end
+
+vim.api.nvim_create_user_command('Floatwindow', create_floating_window, {})
+vim.api.nvim_create_user_command('Toggleterminal', toggle_terminal, {})

e8ffa06f7b94506607d20a407d273a5528311db3
vgitv <33827923+vgitv@users.noreply.github.com>
2024-12-23 16:54:52 +0100

    more telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,14 +5,20 @@ return {
         'nvim-lua/plenary.nvim',
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
-    opts = {},
+    opts = {
+        extensions = {
+            fzf = {}
+        }
+    },
+    config = function()
+        require('telescope').load_extension('fzf')
+    end,
     cmd = 'Telescope',
     keys = {
         { '<Leader>sh', ':Telescope help_tags<CR>', desc = '[S]earch [H]elp',  silent = true },
         { '<leader>sf', ':Telescope find_files<CR>', desc = '[S]earch [H]elp' },
         { '<leader>sb', ':Telescope git_branches<CR>', desc = '[S]earch git [B]ranches' },
         { '<leader>sr', ':Telescope lsp_references<CR>', desc = '[S]earch LSP [R]eferences' },
-        -- FIXME
-        -- { '<leader>sc', function() telescope.builtin.find_files({ cwd = vim.fn.stdpath("config") }) end, desc = '[S]earch neovim [C]onfig' },
+        { '<leader>sc', function() require('telescope.builtin').find_files({ cwd = vim.fn.stdpath("config") }) end, desc = '[S]earch neovim [C]onfig' },
     }
 }

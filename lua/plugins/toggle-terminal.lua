c4fe26b4d71d110a09fc7120727ca12f282fabcf
vgitv <33827923+vgitv@users.noreply.github.com>
2024-12-30 10:21:08 +0100

    toggle term updates

--- lua/plugins/toggle-terminal.lua
+++ lua/plugins/toggle-terminal.lua
@@ -3,12 +3,13 @@ return {
     branch = 'feature/init',
     cmd = 'Toggleterminal',
     keys = {
-        { '<Leader>k', ':Toggleterminal<CR>', desc = 'Toggle main terminal window' },
+        { '<Leader>k', ':Toggleterminal<CR>', desc = 'Toggle main terminal window', silent = true },
     },
     opts = {
         bg_color = '#16161d',  -- main terminal background color
         number = false,  -- no number in main terminal window
         relativenumber = false,  -- no relative number in main terminal window
+        cursorline = true,  -- cursor line in main terminal window
         startinsert = true,  -- start insert mode at term opening
         relative_height = 0.35,  -- relative height of the terminal window (beetween 0 and 1)
     },

bde9c4f623f59bee92de060a118274f11d71c758
vgitv <33827923+vgitv@users.noreply.github.com>
2025-01-03 15:44:50 +0100

    toggle term config

--- lua/plugins/toggle-terminal.lua
+++ lua/plugins/toggle-terminal.lua
@@ -3,14 +3,18 @@ return {
     branch = 'feature/init',
     cmd = 'Toggleterminal',
     keys = {
-        { '<Leader>k', ':Toggleterminal<CR>', desc = 'Toggle main terminal window', silent = true },
+        { '<Leader>k', ':Toggleterminal<CR>', desc = 'Toggle terminal (default height)', silent = true },
+        { '<Leader>f', ':Toggleterminal 0.8<CR>', desc = 'Toggle terminal (80% height)', silent = true },
     },
     opts = {
         bg_color = '#16161d',  -- main terminal background color
-        number = false,  -- no number in main terminal window
-        relativenumber = false,  -- no relative number in main terminal window
-        cursorline = true,  -- cursor line in main terminal window
         startinsert = true,  -- start insert mode at term opening
         relative_height = 0.35,  -- relative height of the terminal window (beetween 0 and 1)
+        local_options = {
+            number = false,  -- no number in main terminal window
+            relativenumber = false,  -- no relative number in main terminal window
+            cursorline = false,  -- cursor line in main terminal window
+            colorcolumn = '',  -- color column
+        }
     },
 }

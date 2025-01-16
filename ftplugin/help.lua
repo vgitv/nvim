d65e3cd551894fe3cfea510f8acf413a1f90522f
vgitv <33827923+vgitv@users.noreply.github.com>
2025-01-16 08:59:20 +0100

    automatically generate tags file

---
+++ ftplugin/help.lua
@@ -0,0 +1,10 @@
+vim.api.nvim_create_autocmd(
+    'BufWritePost',
+    {
+        desc = 'Generate tags file',
+        group = vim.api.nvim_create_augroup('help_ftplugin_group', { clear = true }),
+        callback = function()
+            vim.cmd("helptags " .. vim.fn.expand("%:p:h"))
+        end,
+    }
+)

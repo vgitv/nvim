a96ec19b0934c01653cbb04261c1ecb44829a004
vgitv <33827923+vgitv@users.noreply.github.com>
2025-10-11 09:19:01 +0200

    clean

--- lua/plugins/bullets.vim.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "bullets-vim/bullets.vim",
-    enabled = false,
-    -- only load this plugin for markdown filetype
-    ft = "markdown",
-    keys = {
-        { "<LocalLeader>\\", ":ToggleCheckbox<CR>", desc = "Toggle check box" },
-    },
-}

e6a029bdf72e9b0aa43f3c8724621eb836f238cd
vgitv <33827923+vgitv@users.noreply.github.com>
2025-10-19 07:23:32 +0200

    feat: diagnostic messages

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,5 @@
 return {
     "nvim-telescope/telescope.nvim",
-    version = "*",
     dependencies = {
         "nvim-lua/plenary.nvim",
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },

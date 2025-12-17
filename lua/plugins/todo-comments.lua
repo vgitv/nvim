b1050d89ae9a139c79dc2e76c1a9ceb6e2ca7fc3
vgitv <33827923+vgitv@users.noreply.github.com>
2025-12-17 08:39:44 +0100

    todo comments opts

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -3,5 +3,14 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
         signs = false,
+        keywords = {
+            NOTE = {
+                alt = { "INFO", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10" },
+            },
+        },
+        highlight = {
+            multiline = false,
+            multiline_context = 1,
+        },
     },
 }
